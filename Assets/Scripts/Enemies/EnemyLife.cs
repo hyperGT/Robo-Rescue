cf10002c173bf4cf839e38683190c44941b5d093
Gabriel GTX <63520287+hyperGT@users.noreply.github.com>
2023-05-25 21:02:50 -0300

    Enemy Life 2.0

--- Assets/Scripts/Enemies/EnemyLife.cs
+++ Assets/Scripts/Enemies/EnemyLife.cs
@@ -10,17 +10,33 @@ public class EnemyLife : MonoBehaviour
     [Header("Points")]
     [SerializeField] private int Points;
 
+    [Header("Components")]
+    [SerializeField] private GameObject Explosion;
+    [SerializeField] private GameObject PointsObj;
+
+    [Header("Points Spawn Time")]
+    [SerializeField] private float InvokeTime = 0.5f;
+
     private int BulletDmg;
 
     private void Update()
     {
         BulletDmg = BulletDamage.CurrentBulletDamage;
         if (EnemyHealth <= 0) 
-        { 
+        {
+            Invoke("PointsObjSpawn", InvokeTime);
+            Instantiate(Explosion, transform.position, Quaternion.identity);            
             Destroy(gameObject);
+
             UIController.instance.CurrentScorePlayer += Points;
         } 
     }
+
+    private void PointsObjSpawn()
+    {
+        Instantiate(PointsObj, transform.position, Quaternion.identity);
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
